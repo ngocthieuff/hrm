166b5319bb26bcd795aa28f7aa206d606a84534f
ngocthieuff <ptnngoc22@gmail.com>
2022-12-25 13:51:54 +0700

    rm redundant

--- web-application/src/App.tsx
+++ web-application/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="/logo.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          HRM platform
         </p>
         <a
           className="App-link"
