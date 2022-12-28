2166b3d807a71b7a0e702332d61ab158bea8ec3c
ngocthieuff <ptnngoc22@gmail.com>
2022-12-28 23:44:51 +0700

    rm redundant google variables

--- web-application/src/App.jsx
+++ web-application/src/App.jsx
@@ -1,32 +1,9 @@
-import React, { useEffect } from 'react';
-import jwt_decode from "jwt-decode"
 import './App.css';
 import { LoadedList } from './pages/LoadedList';
 import { LandingPage } from './pages/LandingPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  function handleCallbackResponse(response) {
-    console.log('response', response.credential)
-    let usedObj = jwt_decode(response.credential)
-    console.log('usedObj', usedObj)
-  }
-
-  useEffect(() => {
-    /* global google */ 
-    google.accounts.id.initialize({
-      client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-      callback: handleCallbackResponse
-    })
-
-    google.accounts.id.renderButton(
-      document.getElementById('signInDiv'),
-      { theme: 'outline', size: 'large'}
-    )
-
-    google.accounts.id.prompt()
-  }, [])
 
   return (
     <BrowserRouter>
