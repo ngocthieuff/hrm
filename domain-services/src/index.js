e4ef39bc1fcc471cdddb97e52d9c8c4b926cb614
ngocthieuff <ptnngoc22@gmail.com>
2023-01-23 13:36:21 +0700

    add middleware syntax

---
+++ domain-services/src/index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+app.use(express.urlencoded())
+/*
+express.json() is a method inbuilt in express to 
+recognize the incoming Request Object as a JSON Object. 
+This method is called as a middleware in your application using the code: 
+app.use(express.json());
+
+express.urlencoded() 
+is a method inbuilt in express to 
+recognize the incoming Request Object as strings or arrays. 
+This method is called as a middleware in your application using the code: 
+app.use(express.urlencoded());
+*/
+
+app.listen(PORT, () => console.log(`running express server on PORT ${PORT}`))
+
+const groceries = [
+    {
+        item: 'ngoc',
+        quantity: 1
+    }, 
+    {
+        item: 'milk',
+        quantity: 9
+    }
+]
+
+app.get('/groceries', 
+    (req, res, next) => {
+        console.log('before handling request')
+        next()
+    }, 
+    (req, res, next) => {
+        res.send(groceries)
+        next()
+    },
+    () => {
+        console.log('Finished')
+    }
+)
+
+app.post('/groceries', (req, res) => {
+    console.log(req.body)
+    res.send(201)
+})
